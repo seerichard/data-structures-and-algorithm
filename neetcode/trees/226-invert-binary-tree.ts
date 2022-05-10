69e12af99450fbdf3da53f111ccc177983c34f5a
seerichard <ricardosee@gmail.com>
2022-05-10 22:16:12 +0100

    Remove TreeNode definition

--- neetcode/trees/226-invert-binary-tree.ts
+++ neetcode/trees/226-invert-binary-tree.ts
@@ -1,17 +1,5 @@
 // https://leetcode.com/problems/invert-binary-tree/
 
-// Definition for a binary tree node.
-class TreeNode {
-  val: number;
-  left: TreeNode | null;
-  right: TreeNode | null;
-  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.left = left === undefined ? null : left;
-    this.right = right === undefined ? null : right;
-  }
-}
-
 // Time Complexity:  O(n)
 // Space Complexity: O(h) - h being the height of the tree
 function invertTree(root: TreeNode | null): TreeNode | null {
